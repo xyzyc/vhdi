ee9ffda0ae035abe2ce01f2813ac81c78da80de1
xyzyc <xyzyc@users.noreply.github.com>
2023-04-24 15:25:43 -0500

    the previous random.pi is not random

--- R/random.pi.R
+++ R/random.pi.R
@@ -1,19 +1,16 @@
-# Generated from rdhi.Rmd: do not edit by hand
-
-#' Calculate shortest prediction interval
+#' Calculate prediction interval based on random position.
 #'
 #' @param x data
 #' @param alpha nominal error rate
-#' @param beta a random position. beta must be less than or equal to alpha.
-#' @export
+#' @param beta the left tail probability chosen at random. must be less than or equal to alpha.
 #' @examples
 #' \dontrun{
 #' example_data <- rnorm(500, 0, 5)
 #' random.pi(x, alpha = 0.05, beta = 0.01)
 #' }
 #'
-random.pi <- function(x, alpha, beta){
-  if(is.null(beta)) beta = alpha/2
+random.pi <- function(x, alpha, beta = alpha/2){
+  if(is.null(beta)) beta = runif(1, 0, alpha)
   if(beta > alpha) stop("Invalid beta!")
   lower_q = lower.quantile(c(x, Inf), prob = beta, w = rep(1, length(x)+1), sorted = FALSE)
   upper_q = upper.quantile(c(x, Inf), prob = beta + (1 - alpha), w = rep(1, length(x)+1), sorted = FALSE)
